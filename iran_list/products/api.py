cfcc24cb03970f513ab7f94f4cce8ea8d5be0a09
xishma <xishma@gmail.com>
2017-02-25 19:10:10 +0330

    complete api functions

--- iran_list/products/api.py
+++ iran_list/products/api.py
@@ -112,24 +112,26 @@ def all_products(request):
     return JSONResponse(data)
 
 
-@api_view(['POST'])
+# @api_view(['POST'])
+@csrf_exempt
 def signup(request):
     """
     :param request: first_name, email, password, confirm_password
     :return: success, datails
     """
-    if request.user.is_authenticated():
-        data = {'success': False, 'detail': 'Already logged in!'}
-
+    if request.method != "POST":
+        return JSONResponse({'success': False, 'detail': 'Invalid method!'})
+    elif request.user.is_authenticated():
+        return JSONResponse({'success': False, 'detail': 'Already logged in!'})
     else:
         signup_form = SignupForm(request.data)
 
         if signup_form.is_valid():
             user = signup_form.save()
             if user:
-                data = {'success': True, 'detail': _('Successfully Registered. You can login now.')}
+                data = {'success': True}
             else:
-                data = {'success': False, 'detail': _("Unknown errors during signup. Please try again.")}
+                return JSONResponse({'success': False, 'detail': _("Unknown errors during signup. Please try again.")})
         else:
             data = {'success': False, 'detail': dict(signup_form.errors.items())}
 
@@ -138,172 +140,192 @@ def signup(request):
     return JSONResponse(data)
 
 
+# @api_view(['POST'])
+@csrf_exempt
 def signin(request):
-    if request.user.is_authenticated():
-        return HttpResponseRedirect(reverse('home'))
-
-    login_form = LoginForm()
-    if request.method == 'POST':
+    """
+        :param request: email, password
+        :return: success, datails
+    """
+    if request.method != "POST":
+        return JSONResponse({'success': False, 'detail': 'Invalid method!'})
+    elif request.user.is_authenticated():
+        return JSONResponse({'success': False, 'detail': 'Already logged in!'})
+    else:
         login_form = LoginForm(request.POST)
 
         if login_form.is_valid():
             user = login_form.user
             if user is not None:
                 login(request, user)
-
                 if Profile.objects.filter(user_id=user.id).count() == 0:
                     Profile.objects.create(user=user)
+                data = {'success': True}
+            else:
+                return JSONResponse({'success': False, 'detail': 'Failed to login. Please try again!'})
+        else:
+            data = {'success': False, 'detail': dict(login_form.errors.items())}
 
-                return HttpResponseRedirect(reverse('home'))
-
-    data = {'form': login_form, 'title': _(u'Enter Iran List'), 'gp_client_id': settings.GOOGLE_OAUTH2_CLIENT_ID}
+    data['gp_client_id'] = settings.GOOGLE_OAUTH2_CLIENT_ID
     data = pack_data(request, data)
-    return render_to_response('users/login.html', data)
+    return JSONResponse(data)
 
 
+@csrf_exempt
 def google_signin(request):
-    if request.user.is_authenticated():
-        return HttpResponseRedirect(reverse('home'))
+    if request.method != "POST":
+        data = {'success': False, 'detail': 'Invalid method!'}
+    elif request.user.is_authenticated():
+        data = {'success': False, 'detail': 'Already logged in!'}
 
-    # (Receive token by HTTPS POST)
-    token = request.POST.get("idtoken", "")
-    data = {'success': True, 'user': True, 'token': token}
-    try:
-        idinfo = client.verify_id_token(token, settings.GOOGLE_OAUTH2_CLIENT_ID)
-
-        if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-            raise crypt.AppIdentityError("Wrong issuer.")
-
-            # If auth request is from a G Suite domain:
-            # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
-            #    raise crypt.AppIdentityError("Wrong hosted domain.")
-    except crypt.AppIdentityError:
-        # Invalid token
-        data['success'] = False
-
-    userid = idinfo['sub']
-
-    # check if user is not in the social login table then create it, else just log him in
-    try:
-        social_user = SocialLogin.objects.get(social_unique_id=userid)
-
-    except SocialLogin.DoesNotExist:
-
-        user = User(first_name=idinfo['given_name'], last_name=idinfo['family_name'], email=idinfo['email'],
-                    username=idinfo['email'], password=User.objects.make_random_password())
-        user.save()
+    else:
+        # (Receive token by HTTPS POST)
+        token = request.POST.get("idtoken", "")
+        data = {'success': True, 'user': True, 'token': token}
 
         try:
-            social_user = SocialLogin(user=user, social_unique_id=userid)
-            social_user.save()
-        except:
-            user.delete()
+            idinfo = client.verify_id_token(token, settings.GOOGLE_OAUTH2_CLIENT_ID)
 
-    if social_user is not None:
-        user = User.objects.get(id=social_user.user_id)
-        if user is not None:
-            login(request, user)
-            print(user.is_authenticated)
-    else:
-        data['success'] = False
-        print('user none')
+            if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                raise crypt.AppIdentityError("Wrong issuer.")
+                # If auth request is from a G Suite domain:
+                # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
+                #    raise crypt.AppIdentityError("Wrong hosted domain.")
 
-    mimetype = 'application/json'
-    return HttpResponse(json.dumps(data), mimetype)
+        except crypt.AppIdentityError:
+            # Invalid token
+            return JSONResponse({'success': False})
+
+        userid = idinfo['sub']
+
+        social_user = None
+        # check if user is not in the social login table then create it, else just log him in
+        try:
+            social_user = SocialLogin.objects.get(social_unique_id=userid)
+        except SocialLogin.DoesNotExist:
+            user = User(first_name=idinfo['given_name'], last_name=idinfo['family_name'], email=idinfo['email'],
+                        username=idinfo['email'], password=User.objects.make_random_password())
+            user.save()
+
+            try:
+                social_user = SocialLogin(user=user, social_unique_id=userid)
+                social_user.save()
+            except:
+                user.delete()
+
+        if social_user is not None:
+            user = User.objects.get(id=social_user.user_id)
+            if user is not None:
+                login(request, user)
+            else:
+                social_user.delete()
+                return JSONResponse({'success': False})
+        else:
+            return JSONResponse({'success': False})
+
+    data = pack_data(request, data)
+    return JSONResponse(data)
 
 
-@login_required
+@csrf_exempt
 def change_password(request):
-    password_form = ChangePasswordForm(request.user)
-    if request.method == 'POST':
+    if request.method != "POST":
+        return JSONResponse({'success': False, 'detail': 'Invalid method!'})
+    elif not request.user.is_authenticated():
+        return JSONResponse({'success': False, 'detail': 'Login required!'})
+    else:
         password_form = ChangePasswordForm(request.user, data=request.POST)
 
         if password_form.is_valid():
             if password_form.save():
-                data = {'message': _(u'Password Successfully changed!'), 'login': True}
-                data.update(csrf(request))
-                return render_to_response('common/message.html', data)
+                data = {'success': True, 'detail': _(u'Password Successfully changed!')}
+            else:
+                return JSONResponse({'success': False, 'detail': _('Failed to change password. Please try again!')})
+        else:
+            return JSONResponse({'success': False, 'detail': dict(password_form.errors.items())})
 
-    data = {'password_form': password_form, 'title': _(u'Change Password')}
     data = pack_data(request, data)
-    return render_to_response('users/change_password.html', data)
+    return JSONResponse(data)
 
 
-@login_required
+@csrf_exempt
 def edit_profile(request):
-    user = request.user
-    user_form = EditUserForm(instance=user)
-    if request.method == 'POST':
+    if request.method != "POST":
+        return JSONResponse({'success': False, 'detail': 'Invalid method!'})
+    elif not request.user.is_authenticated():
+        return JSONResponse({'success': False, 'detail': 'Login required!'})
+    else:
+        user = request.user
         user_form = EditUserForm(request.POST, instance=user)
 
         if user_form.is_valid():
             user_form.save()
-            return HttpResponseRedirect(reverse('home'))
+            data = {'success': True}
+        else:
+            return JSONResponse({'success': False, 'detail': dict(user_form.errors.items())})
 
-    data = {'user_form': user_form, 'title': _(u'Edit Profile')}
     data = pack_data(request, data)
-    return render_to_response('users/edit_profile.html', data)
+    return JSONResponse(data)
 
 
+@csrf_exempt
 def request_reset(request):
-    form = ResetPasswordForm()
-    if request.method == 'POST':
+    if request.method != "POST":
+        return JSONResponse({'success': False, 'detail': 'Invalid method!'})
+    else:
         form = ResetPasswordForm(request.POST)
 
         if form.is_valid():
             sent = form.save()
-            data = {'login': True}
             if not sent:
-                data['message'] = _(u'Whoops! Something went wrong! Please Try again.')
+                return JSONResponse({'success': False, 'detail': 'Whoops! Something went wrong! Please Try again.'})
             else:
-                data['message'] = _(u'We Sent You an Email for Password Reset.')
-            data.update(csrf(request))
-            return render_to_response('common/message.html', data)
+                data = {'success': True, 'detail': _(u'We Sent You an Email for Password Reset.')}
+        else:
+            return JSONResponse({'success': False, 'detail': dict(form.errors.items())})
 
-    data = {'form': form, 'page_header': _(u'Request Password Reset'), 'submit_name': _(u'Reset'),
-            'title': _(u'Forgot Password')}
-    data.update(csrf(request))
     data = pack_data(request, data)
     return render_to_response('users/forgot_password.html', data)
 
 
+@csrf_exempt
 def reset_pass(request, reset_code):
-    try:
-        reset_code = ResetPasswordCode.objects.get(code=reset_code)
-    except ResetPasswordCode.DoesNotExist:
-        data = {'message': _(u'Invalid Link!'), 'login': True}
-        data.update(csrf(request))
-        return render_to_response('common/message.html', data)
+    if request.method != "POST":
+        return JSONResponse({'success': False, 'detail': 'Invalid method!'})
+    else:
+        try:
+            reset_code = ResetPasswordCode.objects.get(code=reset_code)
+        except ResetPasswordCode.DoesNotExist:
+            return JSONResponse({'success': False, 'detail': _(u'Invalid Link!')})
 
-    user = reset_code.profile.user
-
-    password_form = ChangePasswordForm(user, reset=True)
-    if request.method == 'POST':
+        user = reset_code.profile.user
         password_form = ChangePasswordForm(user, reset=True, data=request.POST)
 
         if password_form.is_valid():
             if password_form.save():
                 reset_code.delete()
-                data = {'message': _(u'Password Successfully changed!'), 'login': True}
-                data.update(csrf(request))
-                return render_to_response('common/message.html', data)
+                data = {'success': True, 'detail': _(u'Password Successfully changed!')}
+            else:
+                return JSONResponse({'success': False, 'detail': 'Failed to set password. Please try again!'})
+        else:
+            return JSONResponse({'success': False, 'detail': dict(password_form.errors.items())})
 
-    data = {'password_form': password_form, 'title': _(u'Reset Password')}
     data = pack_data(request, data)
     return render_to_response('users/forgot_password.html', data)
 
 
+@csrf_exempt
 def signout(request):
     logout(request)
-    return HttpResponseRedirect(reverse('home'))
+    return JSONResponse({'success': True})
 
 
+@csrf_exempt
 def add_product(request):
-    form = ProductForm()
-
-    if request.method == 'POST':
+    if request.method != "POST":
+        return JSONResponse({'success': False, 'detail': 'Invalid method!'})
+    else:
         product = Product()
-
         if request.user.is_authenticated:
             product.creator = Profile.get_user_profile(request.user)
         else:
@@ -313,29 +335,29 @@ def add_product(request):
         if form.is_valid():
             product = form.save()
             if product:
-                return HttpResponseRedirect(reverse('add_version', args=[product.id]))
+                data = {'success': True}
+            else:
+                return JSONResponse({'success': False, 'detail': 'Failed to set password. Please try again!'})
+        else:
+            return JSONResponse({'success': False, 'detail': dict(form.errors.items())})
 
-    types = Type.objects.all()
-
-    data = {'form': form, 'types': types}
     data = pack_data(request, data)
-    return render_to_response('products/add.html', data)
+    return JSONResponse(data)
 
 
-def add_version(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
-
-    if product.version is not None and not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('login'))
-
-    last_version = product.last_approved_version()
-    if last_version:
-        initial_data = model_to_dict(last_version)
+@csrf_exempt
+def add_version(request, product_slug):
+    if request.method != "POST":
+        return JSONResponse({'success': False, 'detail': 'Invalid method!'})
     else:
-        initial_data = {}
-    form = VersionForm(initial_data)
+        try:
+            product = Product.objects.get(slug=product_slug)
+        except Product.DoesNotExist:
+            return JSONResponse({'success': False, 'detail': 'Invalid product slug.'})
 
-    if request.method == "POST":
+        if product.version is not None and not request.user.is_authenticated:
+            return JSONResponse({'success': False, 'detail': 'You should login first.'})
+
         version = Version()
         version.version_code = 0
         version.product = product
@@ -347,13 +369,12 @@ def add_version(request, product_id):
         form = VersionForm(request.POST, request.FILES, instance=version)
         if form.is_valid():
             form.save()
-            data = {'message': _(u'Successfully added your update. We will review and apply it asap!'), 'home': True}
-            data.update(csrf(request))
-            return render_to_response('common/message.html', data)
+            data = {'success': True, 'detail': _(u'Successfully added your update. We will review and apply it asap!')}
+        else:
+            return JSONResponse({'success': False, 'detail': dict(form.errors.items())})
 
-    data = {'form': form}
     data = pack_data(request, data)
-    return render_to_response('products/add_version.html', data)
+    return JSONResponse(data)
 
 
 def product_page(request, slug):
@@ -381,29 +402,6 @@ def product_page(request, slug):
 
     comments = product.comments.filter(status="pub")
 
-    if request.method == "POST":
-        if not request.user.is_authenticated:
-            return HttpResponseRedirect(reverse('login'))
-
-        if "rating" in request.POST:
-            if rate is None:
-                rate = Rate()
-                user_profile = Profile.get_user_profile(request.user)
-                rate.user = user_profile
-                rate.user_type = user_profile.is_editor
-                rate.product = product
-            rate_form = RateForm(request.POST, instance=rate)
-            if rate_form.is_valid():
-                rate_form.save()
-
-        elif "comment" in request.POST:
-            comment = Comment()
-            comment.user = Profile.get_user_profile(request.user)
-            comment.product = product
-            comment_form = CommentForm(request.POST, instance=comment)
-            if comment_form.is_valid():
-                comment_form.save()
-
     product_data = ProductSerializer(product).data
     version_data = VersionSerializer(product.last_approved_version()).data
     comments_data = CommentSerializer(comments, many=True).data
@@ -417,16 +415,16 @@ def product_page(request, slug):
     return JSONResponse(data)
 
 
+@csrf_exempt
 def rate_product(request, product_slug):
-    if product_slug.endswith("/"):
-        product_slug = product_slug[:-1]
+    if request.method != "POST":
+        return JSONResponse({'success': False, 'detail': 'Invalid method!'})
+    elif not request.user.is_authenticated():
+        return JSONResponse({'success': False, 'detail': 'Login required!'})
+    else:
+        if product_slug.endswith("/"):
+            product_slug = product_slug[:-1]
 
-    data = {'success': False, 'user': True}
-
-    if not request.user.is_authenticated():
-        data['user'] = False
-
-    elif request.is_ajax() and request.method == "POST":
         try:
             product = Product.objects.get(slug=product_slug, status="pub")
             user_profile = Profile.get_user_profile(request.user)
@@ -446,16 +444,49 @@ def rate_product(request, product_slug):
                 rate.user = user_profile
                 rate.user_type = user_profile.is_editor
                 rate.product = product
+
             rate_form = RateForm(request.POST, instance=rate)
             if rate_form.is_valid():
                 rate_form.save()
-                data['success'] = True
+                data = {'success': True}
+
+            else:
+                return JSONResponse({'success': False, 'detail': dict(rate_form.errors.items())})
 
         except Product.DoesNotExist:
-            pass
+            return JSONResponse({'success': False, 'detail': 'Invalid product slug!'})
 
-    mimetype = 'application/json'
-    return HttpResponse(json.dumps(data), mimetype)
+    return JSONResponse(data)
+
+
+@csrf_exempt
+def review_product(request, product_slug):
+    if request.method != "POST":
+        return JSONResponse({'success': False, 'detail': 'Invalid method!'})
+    elif not request.user.is_authenticated():
+        return JSONResponse({'success': False, 'detail': 'Login required!'})
+    else:
+        if product_slug.endswith("/"):
+            product_slug = product_slug[:-1]
+
+        try:
+            product = Product.objects.get(slug=product_slug, status="pub")
+
+            comment = Comment()
+            comment.user = Profile.get_user_profile(request.user)
+            comment.product = product
+            comment_form = CommentForm(request.POST, instance=comment)
+
+            if comment_form.is_valid():
+                comment_form.save()
+                data = {'success': True}
+            else:
+                return JSONResponse({'success': False, 'detail': dict(comment_form.errors.items())})
+
+        except Product.DoesNotExist:
+            return JSONResponse({'success': False, 'detail': 'Invalid product slug!'})
+
+    return JSONResponse(data)
 
 
 def about(request):
--- iran_list/products/api_urls.py
+++ iran_list/products/api_urls.py
@@ -1,30 +1,31 @@
 from django.conf.urls import url
 from .api import signin, signup, signout, edit_profile, change_password, home, request_reset, reset_pass, add_product, \
-    add_version, product_page, all_products, about, contribute, rate_product, google_signin
+    add_version, product_page, all_products, about, contribute, rate_product, google_signin, review_product
 
 urlpatterns = [
-    url(r'^signup/?$', signup, name='signup'),
-    url(r'^login/?$', signin, name='login'),
-    url(r'^google-login/?$', google_signin, name='google-login'),
-    url(r'^logout/?$', signout, name='logout'),
+    url(r'^signup/?$', signup, name='api_signup'),
+    url(r'^login/?$', signin, name='api_login'),
+    url(r'^google-login/?$', google_signin, name='api_google_login'),
+    url(r'^logout/?$', signout, name='api_logout'),
 
-    url(r'^change-password/?$', change_password, name='change_password'),
-    url(r'^edit-profile/?$', edit_profile, name='edit_profile'),
+    url(r'^change-password/?$', change_password, name='api_change_password'),
+    url(r'^edit-profile/?$', edit_profile, name='api_edit_profile'),
 
-    url(r'^reset-password/([0-9a-f]+)/?$', reset_pass, name='reset_pass'),
-    url(r'^forgot-password/?$', request_reset, name='forgot_pass'),
+    url(r'^reset-password/([0-9a-f]+)/?$', reset_pass, name='api_reset_pass'),
+    url(r'^forgot-password/?$', request_reset, name='api_forgot_pass'),
 
-    url(r'^products/add/?$', add_product, name='add_product'),
-    url(r'^products/(\d+)/versions/add$', add_version, name='add_version'),
-    url(r'^products/rate/(.+)?/$', rate_product, name='rate_product'),
+    url(r'^products/add/?$', add_product, name='api_add_product'),
+    url(r'^products/(\d+)/versions/add$', add_version, name='api_add_version'),
+    url(r'^products/rate/(.+)?/$', rate_product, name='api_rate_product'),
+    url(r'^products/review/(.+)?/$', review_product, name='api_rate_product'),
 
-    url(r'^all/?$', all_products, name='all_products'),
+    url(r'^all/?$', all_products, name='api_all_products'),
 
-    url(r'^about/?$', about, name='about'),
-    url(r'^contribute/?$', contribute, name='contribute'),
+    url(r'^about/?$', about, name='api_about'),
+    url(r'^contribute/?$', contribute, name='api_contribute'),
 
     url(r'^$', home, name='api_home'),
 
-    url(r'^(.+)/?$', product_page, name='product_page'),
+    url(r'^(.+)/?$', product_page, name='api_product_page'),
 
 ]
